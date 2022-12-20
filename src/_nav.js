e54833cc6d7f7063bfa3109ab2b4c9e825110d76
adnan1naeem <adnan1naeem@gmail.com>
2022-12-20 13:23:02 +0500

    fix(resolved issue of core ui admin): resolved issue of core ui admin

---
+++ src/_nav.js
@@ -0,0 +1,36 @@
+import { CNavItem } from '@coreui/react'
+
+const _nav = [
+  {
+    component: CNavItem,
+    name: 'Dashboard',
+    to: '/dashboard',
+  },
+  {
+    component: CNavItem,
+    name: 'Quiz Managment',
+    to: '/',
+  },
+  {
+    component: CNavItem,
+    name: 'User Managment',
+    to: '/',
+  },
+  {
+    component: CNavItem,
+    name: 'LeaderBoard and Air drops',
+    to: '/',
+  },
+  {
+    component: CNavItem,
+    name: 'Push notifications',
+    to: '/',
+  },
+  {
+    component: CNavItem,
+    name: 'Flash Learn',
+    to: '/',
+  },
+]
+
+export default _nav
