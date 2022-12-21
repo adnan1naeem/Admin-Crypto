56268811dd201024139b7c5f58b2d09eb0db0e49
adnan1naeem <adnan1naeem@gmail.com>
2022-12-21 19:34:55 +0500

    fix(resolved issue of sidebar): resolved issue of sidebar

--- src/components/AppBreadcrumb.js
+++ src/components/AppBreadcrumb.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import { useLocation } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux'
 
 const AppBreadcrumb = () => {
   const currentLocation = useLocation().pathname
   const title = currentLocation.replace("/", "")
+  const sidebarShow = useSelector((state) => state.sidebarShow)
+ const dispatch = useDispatch()
+
 
   return (
     <h2 style={{ textTransform: 'capitalize' }}>
+    <i class="cil-energy"/>
+
+    <button  onClick={()=> !sidebarShow? dispatch({ type: 'set', sidebarShow: true }):dispatch({ type: 'set', sidebarShow: false })}>Show</button>
       {title}
     </h2>
   )
