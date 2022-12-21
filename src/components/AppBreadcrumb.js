87a8df622e368aa22d2ba4c1db1fac7a46826a28
adnan1naeem <adnan1naeem@gmail.com>
2022-12-21 19:43:12 +0500

    fix(resolved issue ofsidebar): resolved issue ofsidebar

--- src/components/AppBreadcrumb.js
+++ src/components/AppBreadcrumb.js
@@ -1,19 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useLocation } from 'react-router-dom'
 import { useSelector, useDispatch } from 'react-redux'
 
 const AppBreadcrumb = () => {
   const currentLocation = useLocation().pathname
   const title = currentLocation.replace("/", "")
+  const [size,setSize]=useState();
   const sidebarShow = useSelector((state) => state.sidebarShow)
  const dispatch = useDispatch()
+ React.useEffect(() => {
+  function handleResize() {
+    console.log('resized to: ', window.innerWidth, 'x', window.innerHeight)
+    setSize(window.innerWidth)
+}
 
+  window.addEventListener('resize', handleResize)
+})
 
   return (
     <h2 style={{ textTransform: 'capitalize' }}>
     <i class="cil-energy"/>
 
-    <button  onClick={()=> !sidebarShow? dispatch({ type: 'set', sidebarShow: true }):dispatch({ type: 'set', sidebarShow: false })}>Show</button>
+    {size<=1000?<button  onClick={()=> !sidebarShow? dispatch({ type: 'set', sidebarShow: true }):dispatch({ type: 'set', sidebarShow: false })}>Show</button>:null}
       {title}
     </h2>
   )
