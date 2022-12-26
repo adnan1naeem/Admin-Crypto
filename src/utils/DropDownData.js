506f12223d2de627b28ca9b8171b79bb8f53fab3
saim30 <sshafqat@techtiz.co>
2022-12-26 12:43:44 +0500

    Feature: Quiz Setting

---
+++ src/utils/DropDownData.js
@@ -0,0 +1,62 @@
+export const DropDownData = [
+    {
+        id: "1",
+        value: 1,
+    },
+    {
+        id: "2",
+        value: 2,
+    },
+    {
+        id: "3",
+        value: 3,
+    },
+    {
+        id: "4",
+        value: 4,
+    },
+    {
+        id: "5",
+        value: 5,
+    },
+    {
+        id: "6",
+        value: 6,
+    },
+    {
+        id: "7",
+        value: 7,
+    },
+    {
+        id: "8",
+        value: 8,
+    },
+    {
+        id: "9",
+        value: 9,
+    },
+    {
+        id: "10",
+        value: 10,
+    },
+    {
+        id: "11",
+        value: 11,
+    },
+    {
+        id: "12",
+        value: 12,
+    },
+    {
+        id: "13",
+        value: 13,
+    },
+    {
+        id: "14",
+        value: 14,
+    },
+    {
+        id: "15",
+        value: 15,
+    }
+]
